e0ae85d29a1139e354340098d87004e1859a6f1e
Matt Roberts <vtpanda@users.noreply.github.com>
2018-10-03 10:36:27 -0400

    Changed to use median for imputing values.

--- FirstLesson/decision_tree_classifier.py
+++ FirstLesson/decision_tree_classifier.py
@@ -10,7 +10,9 @@ from sklearn import tree
 #I had to install scipy to be able to use sklearn
 from sklearn import svm
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import Imputer
+#replace Imputer (which is deprecated) with SimpleImputer
+#from sklearn.preprocessing import Imputer
+from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 import graphviz
 from sklearn.model_selection import cross_val_score
@@ -28,12 +30,17 @@ y = df[['Survived']]
 #i'll use the correct columns at the time i need them
 x = df #df[['Pclass','Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']]
 
+#show the distribution of values
+#df['Age'].value_counts()
 #just for testing
 #x[x['Embarked'].isna()]
 #just to show the missing ages
 #x[x['Age'].isna()]
 #still just playing around
 #x.loc[:,'Age']
+#i want to see the counts by age
+#x.groupby('Age').count()[['PassengerId']].to_csv('~/Documents/GitHub/TiberDataScienceLearning/Data/Titanic/ages.csv')
+#*******i really should figure out how to graph this in python, but i will do that later...
 
 #turn missing ages into the mean age
 # imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
@@ -71,11 +78,30 @@ x = df #df[['Pclass','Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']]
 #this is the final set of preprocessing that we're doing; I'm going to move this to do it on each set of data individually
 #actually, i take that back; let's turn this into a function; no reason to duplicate this code
 def preprocessdataframe (df):
-    imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+    imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
     imputer = imputer.fit(df.loc[:,['Age']])
     df.loc[:,'Age'] = imputer.transform(df.loc[:,['Age']])
 
-    imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+    imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
+    imputer = imputer.fit(df.loc[:,['Fare']])
+    df.loc[:,'Fare'] = imputer.transform(df.loc[:,['Fare']])
+
+    df.Embarked = df.Embarked.fillna('S')
+
+    labelencoder_X = LabelEncoder()
+    df.loc[:,'Sex'] = labelencoder_X.fit_transform(df.loc[:,'Sex'])
+    factors = pd.get_dummies(df.loc[:,'Embarked'])
+    df['C'] = factors['C']
+    df['Q'] = factors['Q']
+    df['S'] = factors['S']
+    return df
+
+def preprocessdataframe_median (df):
+    imputer = SimpleImputer(missing_values = np.nan, strategy = 'median')
+    imputer = imputer.fit(df.loc[:,['Age']])
+    df.loc[:,'Age'] = imputer.transform(df.loc[:,['Age']])
+
+    imputer = SimpleImputer(missing_values = np.nan, strategy = 'median')
     imputer = imputer.fit(df.loc[:,['Fare']])
     df.loc[:,'Fare'] = imputer.transform(df.loc[:,['Fare']])
 
@@ -90,6 +116,7 @@ def preprocessdataframe (df):
     return df
 
 
+
 #10-fold
 kf = KFold(n_splits=10)
 kf.get_n_splits(x)
@@ -100,9 +127,9 @@ for train_index, test_index in kf.split(x):
     #i'm not sure if iloc (positional) or loc (Label) is better here; does split return positions or labels?
     cvv_x_train, cvv_x_test = x.iloc[train_index], x.iloc[test_index]
     cvv_y_train, cvv_y_test = y.iloc[train_index], y.iloc[test_index]
-    #this is where I've left off; i guess i need to train each set and get the results, right?
-    cvv_x_train = preprocessdataframe(cvv_x_train)
-    cvv_x_test = preprocessdataframe(cvv_x_test)
+
+    cvv_x_train = preprocessdataframe_median(cvv_x_train)
+    cvv_x_test = preprocessdataframe_median(cvv_x_test)
 
     clfkfold = clfkfold.fit(cvv_x_train[['Pclass','Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'C', 'Q', 'S']], cvv_y_train)
     cvv_y_predictions = clfkfold.predict(cvv_x_test[['Pclass','Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'C', 'Q', 'S']])
@@ -120,8 +147,10 @@ for train_index, test_index in kf.split(x):
 #split off 20%
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
 
-x_train = preprocessdataframe(x_train)
-x_test = preprocessdataframe(x_test)
+
+
+x_train = preprocessdataframe_median(x_train)
+x_test = preprocessdataframe_median(x_test)
 
 #train decision tree (??)
 clf = tree.DecisionTreeClassifier()
@@ -141,7 +170,7 @@ dot_data = tree.export_graphviz(clf, out_file=None,
 graph = graphviz.Source(dot_data)
 #had to do "brew install graphviz" to get the following statement to work
 #this was put one folder up; i manually put these files in the FirstLesson subfolder
-graph.render("Titanic_Separate_Columns")
+graph.render("Titanic_Separate_Columns_Median")
 
 #do 10-fold cross-validation;
 #this seems to score for both positive and negative rather than just for positive, so I'm not going to use it
@@ -188,7 +217,7 @@ testx = pd.read_csv('~/Documents/GitHub/TiberDataScienceLearning/Data/Titanic/te
 
 
 
-testx = preprocessdataframe(testx)
+testx = preprocessdataframe_median(testx)
 
 testpredictions = clf.predict(testx[['Pclass','Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'C', 'Q', 'S']])
 outputdata = DataFrame()
--- README.md
+++ README.md
@@ -6,6 +6,12 @@ Titanic: https://www.kaggle.com/c/titanic/data
 
 Telco: https://community.watsonanalytics.com/wp-content/uploads/2015/03/WA_Fn-UseC_-Telco-Customer-Churn.csv
 
+Update #3: Okay, I'm now using median, but looking at the distribution of values for Age, there's very little difference between using mean and median; that said, I might as well keep that code around for future problems.  Also, I used Rachel's code as a pointer to get counts by Age, and I exported that data to Excel to view in graph form.  Next thing to learn is how to create graphs in Python so I don't have to export to Excel.  Also, at some point, I want to figure out how to create normalized values for Age as a code exercise.
+
+Update #2: Okay, I figured out why I didn't get any deprecation warnings from using Imputer; I was using scikit-learn version 0.19.  I upgraded to scikit-learn 0.20.  I'm now using SimpleImputer
+
+Update on 10/3/2018: So, reading the Python docuementation, it looks like the Imputer class is deprecated and will be removed in a future version.  Need to update that with something.  
+
 Update on 9/30/2018: Fixed issue with submission file
 
 Update on 9/29/2018: I've updated the decision_tree_classifier.py file to do preprocessing after splitting, and also to split the embarked field into three columns.  I'm also outputting a file for submission.
