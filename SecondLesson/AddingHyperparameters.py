b0551b524949642d595494c11237e90ab4779a2f
Matt Roberts <vtpanda@users.noreply.github.com>
2018-10-15 23:10:52 -0400

    Added initial draft of the week 2 assignment. Just the basic code without any changes.

---
+++ SecondLesson/AddingHyperparameters.py
@@ -0,0 +1,90 @@
+#!/usr/local/bin/python3
+
+import pandas as pd
+from sklearn import metrics
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+import numpy as np
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import KFold
+import graphviz
+import matplotlib.pyplot as plt
+from sklearn.model_selection import cross_val_score
+
+def preprocessdataframe (df):
+    imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
+    imputer = imputer.fit(df.loc[:,['Age']])
+    df.loc[:,'Age'] = imputer.transform(df.loc[:,['Age']])
+
+    imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
+    imputer = imputer.fit(df.loc[:,['Fare']])
+    df.loc[:,'Fare'] = imputer.transform(df.loc[:,['Fare']])
+
+    df.Embarked = df.Embarked.fillna('S')
+
+    df = pd.get_dummies(data=df, columns=['Embarked', 'Pclass', 'Sex'])
+
+    return df
+
+def showdecisiontree(model):
+    dot_data = tree.export_graphviz(model, out_file=None,
+         feature_names=['Age', 'SibSp', 'Parch', 'Fare', 'Embarked_C', 'Embarked_Q',
+            'Embarked_S', 'Pclass_1', 'Pclass_2', 'Pclass_3', 'Sex_female',
+            'Sex_male'],
+         class_names=['Did not survive', 'Survived'],
+         filled=True, rounded=True,
+         special_characters=True)
+    graph = graphviz.Source(dot_data)
+    graph.render("Titanic_Decision_Tree")
+    return True
+
+def showroccurve(fpr, tpr, roc_auc):
+    plt.figure()
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange',
+             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.show()
+
+
+df = pd.read_csv('~/Documents/GitHub/TiberDataScienceLearning/Data/Titanic/train.csv')
+y = df[['Survived']]
+x = df[['Pclass','Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']]
+
+#split off 20%
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+x_train = preprocessdataframe(x_train)
+x_test = preprocessdataframe(x_test)
+
+
+clf = tree.DecisionTreeClassifier()
+model = clf.fit(x_train, y_train)
+y_predictions = model.predict(x_test)
+y_predictions_proba = model.predict_proba(x_test)
+
+cm = pd.DataFrame(
+    confusion_matrix(y_test, y_predictions),
+    columns=['Predicted Not Survival', 'Predicted Survival'],
+    index=['True Not Survival', 'True Survival']
+)
+
+score = model.score(x_test, y_test)
+
+fpr, tpr, thresholds = metrics.roc_curve(y_test, y_predictions)
+roc_auc = metrics.auc(fpr, tpr)
+
+cross_val_roc = cross_val_score(model, X=x_train, y=y_train, cv=10, scoring='roc_auc')
+roc_score = np.mean(cross_val_roc)
+#
+# clfkfold = tree.DecisionTreeClassifier()
+#
+# kf = KFold(n_splits=10)
+# kf.get_n_splits(x)
